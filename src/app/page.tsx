16228fdd67986188522b6415019ca6c430fe5172
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2024-04-24 02:31:23 +0100

    fix: tooltip for both random and memorable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import FAQ from "@/components/templates/home-page/faq";
 import Footer from "@/components/templates/home-page/footer";
 import Hero from "@/components/templates/home-page/hero";
 import WhyPassSafe from "@/components/templates/home-page/why-passSafe";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="mt-[107px] md:mt-[150px] lg:mt-[197px]">
-      <Hero />
-      <WhyPassSafe />
-      <FAQ />
-      <Footer />
+      <Suspense fallback={<>Loading...</>}>
+        <Hero />
+        <WhyPassSafe />
+        <FAQ />
+        <Footer />
+      </Suspense>
     </main>
   );
 }
