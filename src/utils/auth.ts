b935bd3b51c78cd8200d1aeb42f8f4ffb8bce8aa
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2024-06-10 09:16:53 +0100

    passSafe name changed accress ui

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,10 +1,10 @@
 const auth = () => {
   const handleLogin = () => {
-    window.open("https://seampass-be.up.railway.app/", "_blank");
+    window.open("https://seampass-fe.up.railway.app/", "_blank");
   };
 
   const handleCreateAccount = () => {
-    window.open("https://seampass-be.up.railway.app/create-account", "_blank");
+    window.open("https://seampass-fe.up.railway.app/create-account", "_blank");
   };
 
   return { handleLogin, handleCreateAccount };
